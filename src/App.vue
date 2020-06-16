04a7e69333ac61a7b5604f3da859cdf1dd1f3d4f
juanwmedia <juan@wmedia.es>
2020-06-16 13:53:45 +0200

    Route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <AppHeader />
-    <RouterView />
+    <transition name="fade">
+      <RouterView />
+    </transition>
   </div>
 </template>
 
@@ -30,4 +32,46 @@ body {
 body {
   font-family: sans-serif;
 }
+</style>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  opacity: 1;
+  transition: all 0.5s ease-in-out;
+}
+
+.fade-enter,
+.fade-leave {
+  opacity: 0;
+}
+/* 
+.fade-enter-active {
+  animation: fade 0.2s ease-in-out;
+}
+
+@keyframes fade {
+  from {
+    opacity: 0;
+  }
+  50% {
+    opacity: 50%;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+.fade-leave-active {
+  animation: slide 0.2s ease-in-out;
+}
+
+@keyframes slide {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-300px);
+  }
+} */
 </style>
